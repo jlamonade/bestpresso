cdccd7a1f2e3e382fb319ed01b6a9e45256a8c79
Jason Lam <nyjasonlam@gmail.com>
2021-06-12 19:37:04 -0400

    implemented rudimentary user login

---
+++ api/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      require: 'Please enter a username.'
+    },
+    email: {
+      type: String,
+      trim: true,
+      require: 'Please enter a valid email.',
+      match: [/.+\@.+\..+/],
+      unique: true
+    },
+    password: {
+      type: String,
+      trim: true,
+      require: 'Please enter a valid password.'
+    }
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST users listing. */
+/* for registering users */
+router.post('/register', async (req, res, next) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 10)
+    const result = await User.create(req.body)
+    res.send(result)
+  } catch (err) {
+    if (err) res.status(500).send(err)
+  }
 });
+
+/* POST users listing. */
+/* Logging in route */
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return res.status(400).send({ message: 'User does not exist' })
+    }
+    if(!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(400).send({ message: 'Incorrect password'})
+    }
+    // if email and password are correct then
+    res.send({ message: 'Username and password are correct!' })
+  } catch (err) {
+    res.status(500).send(err)
+  }
+})
+
+router.get('/', async (req, res) => {
+  const userData = await User.find({})
+  res.json(userData)
+})
 
 module.exports = router;
