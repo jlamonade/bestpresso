55d5bdcf8da6fe61eb482e2c2e5eaeb6235cbf72
Jason <nyjasonlam@gmail.com>
2021-06-12 23:28:54 -0400

    added create bean route

--- api/routes/espresso.js
+++ api/routes/espresso.js
@@ -9,7 +9,10 @@ router.get('/', async (req, res) => {
 
 // create bean
 router.post('/', async (req, res) => {
-
+  req.body.user = req.session.passport.user
+  const beanData = await Bean.create(req.body)
+  if (!beanData[0]) res.json(beanData)
+  else res.json("Bean Not Added.")
 })
 
 module.exports = router
