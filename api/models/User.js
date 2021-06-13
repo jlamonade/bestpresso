a9d28cb95f8f284531b0c449c3341deadb7f8511
Jason Lam <nyjasonlam@gmail.com>
2021-06-12 21:20:10 -0400

    linted code

--- api/models/User.js
+++ api/models/User.js
@@ -1,29 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      require: 'Please enter a username.'
-    },
-    email: {
-      type: String,
-      trim: true,
-      require: 'Please enter a valid email.',
-      match: [/.+\@.+\..+/],
-      unique: true
-    },
-    password: {
-      type: String,
-      trim: true,
-      require: 'Please enter a valid password.'
-    }
-  }
-)
+const userSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    require: "Please enter a username.",
+  },
+  email: {
+    type: String,
+    trim: true,
+    require: "Please enter a valid email.",
+    match: [/.+\@.+\..+/],
+    unique: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    require: "Please enter a valid password.",
+  },
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
